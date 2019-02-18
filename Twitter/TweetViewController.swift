f26a6308e4e082d24d60b690841c535eeffc5854
Irais Gopar osorio <iraisgoparosorio@Iraiss-MacBook-Pro.local>
2019-02-18 15:08:25 -0800

    creating tweets part 1

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,47 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Irais Gopar osorio on 2/18/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+    
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CancelButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func TweetButton(_ sender: Any) {
+        if(!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting Tweet!  \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        }else{
+            self.dismiss(animated: true, completion: nil) 
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
